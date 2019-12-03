d8d98ca32c36c83c0ec457acd0431cc8676d2c30
Samuglz6 <samu_sgl_96@hotmail.com>
2019-12-03 19:41:03 +0100

    application of a neural network (not finished)

--- Main.py
+++ Main.py
@@ -2,7 +2,17 @@ from NeuralNetwork import NeuralNetwork
 import numpy as np
 
 if __name__ == '__main__':
-    network = NeuralNetwork()
-    inputs = np.array([2,3])
+    train = np.array([[-8, -1],  #Alice
+                    [19, 6],   #Bob
+                    [11, 4],   #Charlie
+                    [-21, -6],]) #Diana
+    expected = np.array([[1,    #Alice
+                         0,    #Bob
+                         0,    #Chalie
+                         1],])   #Diana
 
-    print(network.activate(inputs))
+    network = NeuralNetwork()
+    network.train(train, expected)
+
+    input = np.array([-8,-1])
+    #print(network.feedforward(input))
--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -1,19 +1,95 @@
-from Neuron import Neuron
 import numpy as np
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+def deriv_sigmoid(x):
+    return x * (1 - x)
+
+def squad_error(obtained, expected):
+    return ((expected - obtained)**2)
 
 class NeuralNetwork:
     def __init__(self):
-        weights = np.array([0,1])
-        bias = 0
+        #Inicializamos los pesos para cada rama de forma aleatoria
+        #Las 2 neuronas ocultas h1 y h2 tienen 2 entradas (2 pesos) y la
+        #neurona de salida o1 otras 2 entradas.
+        wh1 = [np.random.normal(), np.random.normal()]
+        wh2 = [np.random.normal(), np.random.normal()]
+        wo1 = [np.random.normal(), np.random.normal()]
 
-        self.h1 = Neuron(weights, bias)
-        self.h2 = Neuron(weights, bias)
-        self.o1 = Neuron(weights, bias)
+        #Creamos las neuronas h1 y h2 de la capa oculta y o1 de la capa de salida
+        self.h1 = Neuron(wh1, np.random.normal())
+        self.h2 = Neuron(wh2, np.random.normal())
+        self.o1 = Neuron(wo1, np.random.normal())
 
-    def activate(self, inputs):
-        out_h1 = self.h1.activate(inputs)
-        out_h2 = self.h2.activate(inputs)
+    def feedforward(self, x):
+        sum_h1 = self.h1.weights[0] * x[0] + self.h1.weights[1] * x[1] + self.h1.bias
+        f_h1 = self.h1.activationFunction(sum_h1)
 
-        out_o1 = self.o1.activate(np.array([out_h1, out_h2]))
+        sum_h2 = self.h2.weights[0] * x[0] + self.h2.weights[1] * x[1] + self.h2.bias
+        f_h2 = self.h2.activationFunction(sum_h2)
 
-        return out_o1
+        sum_o1 = self.o1.weights[0] * x[0] + self.o1.weights[1] * x[1] + self.o1.bias
+        result = self.o1.activationFunction(sum_o1)
+
+        return result
+
+    def train(self, input, expected):
+        learn_rate = 0.1
+        epochs = 1
+
+        for epoch in range(epochs):
+            for x, y in zip(input, expected):
+                 #FEEDFORWARD
+                 sum_h1 = self.h1.weights[0] * x[0] + self.h1.weights[1] * x[1] + self.h1.bias
+                 f_h1 = self.h1.activationFunction(sum_h1)
+
+                 sum_h2 = self.h2.weights[0] * x[0] + self.h2.weights[1] * x[1] + self.h2.bias
+                 f_h2 = self.h2.activationFunction(sum_h2)
+
+                 sum_o1 = self.o1.weights[0] * x[0] + self.o1.weights[1] * x[1] + self.o1.bias
+                 f_o1 = self.o1.activationFunction(sum_o1)
+
+                 #BACKPROPAGATION
+                 e = squad_error(f_o1, y)
+
+                 #Variacion en los pesos de la Neurona o1
+                 delta_o1 = e * deriv_sigmoid(f_o1)
+
+                 d_o1_w1 = learn_rate * delta_o1 * f_h1
+                 d_o1_w2 = learn_rate * delta_o1 * f_h2
+
+                 new_weights_o1 = [(self.o1.weights[0] + d_o1_w1), (self.o1.weights[1] + d_o1_w2)]
+
+                 #Variacion en los pesos de la Neurona h1
+                 delta_h1 = deriv_sigmoid(f_h1) * (delta_o1 * self.o1.weights[0])
+
+                 d_h1_w1 = learn_rate * delta_h1 * x[0]
+                 d_h1_w2 = learn_rate * delta_h1 * x[1]
+
+                 new_weights_h1 = [self.o1.weights[0] + d_o1_w1, self.o1.weights[1] + d_o1_w2]
+
+                 #Variacion en los pesos de la Neurona h2
+                 delta_h2 = deriv_sigmoid(f_h2) * (delta_o1 * self.o1.weights[1])
+
+                 d_h2_w1 = learn_rate * delta_h2 * x[0]
+                 d_h2_w2 = learn_rate * delta_h2 * x[1]
+
+                 new_weights_h2 = [self.o1.weights[0] + d_o1_w1, self.o1.weights[1] + d_o1_w2]
+
+                 #Actualizamos los pesos de las neuronas
+                 print(delta_h1)
+                 print(self.o1.weights)
+                 self.o1.weights = new_weights_o1
+                 self.h1.weights = new_weights_h1
+                 self.h2.weights = new_weights_h2
+        return 0
+
+class Neuron:
+    def __init__(self, weights, bias):
+        self.weights = weights
+        self.bias = bias
+
+    def activationFunction(self, x):
+        return sigmoid(x)
